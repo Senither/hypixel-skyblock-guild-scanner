93b2cf3958a92190b10dc0ec289a4555eaf725c8
Alexis Tan <alexis@sen-dev.com>
2021-01-17 23:58:18 +0100

    Add page change transitions

--- assets/js/views/App.vue
+++ assets/js/views/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="h-screen flex flex-col justify-between">
-    <router-view></router-view>
+    <transition-element type="slide">
+      <router-view></router-view>
+    </transition-element>
 
     <footer class="relative mt-12 py-8 px-4 bg-gray-800 border-t-2 border-gray-700 text-center sm:py-10 sm:px-8">
       <p class="mt-2 text-gray-300 text-sm">
         Hypixel Guild Scanner, get the
-        <a href="https://github.com/Senither/hypixel-skyblock-guild-scanner" class="text-indigo-400 hover:text-indigo-500">source code</a>
+        <a
+          href="https://github.com/Senither/hypixel-skyblock-guild-scanner"
+          class="text-indigo-400 hover:text-indigo-500"
+          >source code</a
+        >
         on GitHub.
       </p>
       <p class="text-gray-300 text-xs">
@@ -19,3 +25,13 @@
     </footer>
   </div>
 </template>
+
+<script>
+import TransitionElement from '@/components/TransitionElement'
+
+export default {
+  components: {
+    TransitionElement,
+  },
+}
+</script>
