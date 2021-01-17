2ad96c9b59fe20b766a5ddcf87f3173875c0beac
Alexis Tan <alexis@sen-dev.com>
2021-01-17 23:55:50 +0100

    Add support for searching for guilds in select page view

--- assets/js/views/SelectGuild.vue
+++ assets/js/views/SelectGuild.vue
@@ -1,15 +1,146 @@
 <template>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum fugiat, id minima optio perferendis hic dicta iste consequatur tempora harum, asperiores, aperiam, accusantium possimus fuga eum nulla deserunt rerum repellendus.</p>
+  <div>
+    <section class="pt-16 pb-6">
+      <div class="max-w-screen-xl mx-auto text-center">
+        <h1 class="text-gray-100 text-5xl font-medium leading-tight">
+          Select Guild
+        </h1>
+        <p class="text-gray-500 text-2xl">
+          Enter the name of the guild you want to scan below.
+        </p>
+      </div>
+    </section>
+
+    <section class="pb-6">
+      <div class="max-w-screen-lg mx-auto">
+        <div class="bg-gray-800 px-8 py-6 lg:rounded-md text-base shadow-2xl">
+          <div class="flex">
+            <input
+              ref="name"
+              v-model="name"
+              class="flex-1 mr-0 px-4 py-4 bg-white text-gray-800 border-t border-b border-l border-gray-200 rounded-l-lg outline-none"
+              placeholder="Enter the name of the guild you want to scan here..."
+            />
+            <button
+              @click="lookupGuild"
+              class="px-8 text-white font-bold uppercase bg-gray-900 hover:bg-gray-800 border-gray-700 border-t border-b border-r rounded-r-lg focus:outline-none"
+            >
+              <span class="flex" v-if="isLoading">
+                <loading-icon />
+                Fetching data
+              </span>
+              <span v-else>Lookup Guild</span>
+            </button>
+          </div>
+          <p v-if="error" class="mt-2 text-red-400">
+            {{ error }}
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <transition-element>
+      <section class="pb-8" v-if="guild">
+        <div class="max-w-screen-lg mx-auto">
+          <div class="py-6 text-base text-center">
+            <p>
+              Found a guild called
+              <span class="font-semibold text-indigo-400">[{{ guild.tag }}]</span>
+              <span class="font-semibold text-indigo-300">{{ guild.name }}</span>
+              with
+              <span class="font-semibold text-indigo-300">{{ guild.members.length }}</span>
+              members.
+            </p>
+
+            <p class="text-gray-400" v-if="guild.description">"{{ guild.description }}"</p>
+
+            <p class="pt-10">
+              Do you want to
+              <a @click="scan" class="py-2 px-3 bg-indigo-800 hover:bg-indigo-700 text-white rounded-md cursor-pointer">
+                continue to scan the guild
+              </a>
+              or
+              <a @click="reset" class="py-2 px-3 bg-gray-800 hover:bg-gray-700 text-white rounded-md cursor-pointer">
+                search for another guild to scan
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
+    </transition-element>
+  </div>
 </template>
 
 <script>
+import TransitionElement from '@/components/TransitionElement'
+import LoadingIcon from '@/components/LoadingIcon'
+
 export default {
+  components: {
+    TransitionElement,
+    LoadingIcon,
+  },
+
   mounted() {
     if (this.$store.getters.token == null) {
       return this.$router.push({
         name: 'landing-page',
       })
     }
+  },
+
+  data() {
+    return {
+      isLoading: false,
+      name: '',
+      guild: null,
+      error: null,
+    }
+  },
+
+  methods: {
+    lookupGuild() {
+      if (this.name.trim().length < 3) {
+        return (this.error = 'The guild name must be at least 3 characters or longer!')
+      }
+
+      if (this.isLoading) {
+        return
+      }
+
+      this.error = null
+      this.guild = null
+      this.isLoading = true
+
+      axios
+        .get(`/guild?key=${this.$store.getters.token}&name=${this.name}`)
+        .then(response => {
+          this.isLoading = false
+
+          const { guild } = response.data
+          if (guild == null) {
+            return (this.error = `Found no guild matching your search query. Maybe you entered the name wrong?`)
+          }
+
+          this.guild = guild
+        })
+        .catch(error => {
+          console.error('Unexpected error occurred: ', error)
+
+          this.error = 'Unexpected error occurred: ' + error.message
+        })
+    },
+
+    scan() {
+      // TODO: Continue the scan here...
+    },
+
+    reset() {
+      this.name = ''
+      this.guild = null
+
+      this.$refs.name.focus()
+    },
   },
 }
 </script>
