5a4b5e350e51cb62a020a1bcd284c74cd043d8f8
Alexis Tan <alexis@sen-dev.com>
2021-02-13 20:29:26 +0100

    Containerize app with Docker (#1)
    * Create basic docker setup
    
    The system still needs some work since the API can't really be reached as all the HTTP API requests are technically made by the user visiting the site, and they don't have access to the network the API is hosted under, so need to find a solution for either making the requests internally, or just making the API listen on a public port so it can be accessed by the API.
    
    * Change facade service to be public
    
    This also removes the private network between the containers since it doesn't actually work for what we want to do.
    
    * Add default ports to the app and facade using env variables
    
    * Rename services to match the environment variable used for them
    
    * Add docker setup guide to the readme

---
+++ docker/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  /**
+   * This is the hostname and port that the API facade is running on,
+   * you can use the IP of the server the API is hosted on combined
+   * with the port defined in the config for the API.
+   */
+  facadeBaseUrl: 'http://localhost:9281/'
+}
